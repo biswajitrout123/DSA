37ebf72cb595610cf4ea1ce268e863d862d35a84
Biswajit Rout <routhappy9876@gmail.com>
2026-01-27 00:42:13 +0530

    solved Q-5 of Arrays-D3

--- Practice_Arrays/Day_3/Q_5.java
+++ Practice_Arrays/Day_3/Q_5.java
@@ -4,8 +4,19 @@ public class Q_5 {
     public static void main(String[] args) {
         // Q-5 Difference between max and min
         int[] arr = {8, 6, 9, 10, 5, 6};
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for(int i = 0; i < arr.length; i++) {
-            
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+            if(arr[i] < min){
+                min = arr[i];
+            }
         }
+        int difference = max - min;
+        System.out.println("Minium Element is :" + min);
+        System.out.println("Maximum Element is :" + max);
+        System.out.println("Difference (max-min) is :" + difference);
     }
 }
