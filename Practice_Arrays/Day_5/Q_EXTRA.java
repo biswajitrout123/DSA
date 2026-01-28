c304e88907151edb1bd6075059805c7ab623fa73
Biswajit Rout <routhappy9876@gmail.com>
2026-01-28 14:15:23 +0530

    solve another question of array

--- Practice_Arrays/Day_5/Q_EXTRA.java
+++ Practice_Arrays/Day_5/Q_EXTRA.java
@@ -2,6 +2,24 @@ package Day_5;
 
 public class Q_EXTRA {
     public static void main(String[] args) {
+//  Separate even and odd numbers
+// (keep even first, then odd)
+
+        int[] arr = {8, 6, 7, 3, 5, 2, 9, 0, 11, 20, 10, 80, 50, 34, 33, 13, 43, 42, 60};
+        int k = 0;
         
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] %2 == 0){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        while(arr[i] %2 != 0){
+            arr[k] = arr[i];
+            k++;
+        }
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
