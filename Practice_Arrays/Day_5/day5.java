248744c616d3dc88796926918ed277e979b59e80
Biswajit Rout <routhappy9876@gmail.com>
2026-01-28 10:08:00 +0530

    solved another question of array

--- Practice_Arrays/Day_5/day5.java
+++ Practice_Arrays/Day_5/day5.java
@@ -2,7 +2,21 @@ package Day_5;
 
 public class day5 {
     public static void main(String[] args) {
-        
+        // Reverse an array
+        int[] arr = {8,9,0,10};
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
 
@@ -21,16 +35,16 @@ public class day5 {
 
 // 🎯 Goal: Manipulate arrays confidently
 
-// Reverse an array
+// Q-1 Reverse an array
 
-// Copy one array into another
+// Q-2 Copy one array into another
 
-// Replace all negative numbers with zero
+// Q-3 Replace all negative numbers with zero
 
-// Move all zeros to the end
+// Q- 4 Move all zeros to the end
 
-// Separate even and odd numbers
+// Q-5 Separate even and odd numbers
 
-// Rotate array left by 1
+// Q-6 Rotate array left by 1
 
 // ✅ You should start seeing patterns
