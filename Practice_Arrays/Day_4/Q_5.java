1ce5897db1d7bea54be649f553b729576ab7158d
Biswajit Rout <routhappy9876@gmail.com>
2026-01-28 09:35:50 +0530

    solved next question of array

--- Practice_Arrays/Day_4/Q_5.java
+++ Practice_Arrays/Day_4/Q_5.java
@@ -3,5 +3,35 @@ package Day_4;
 public class Q_5 {
     public static void main(String[] args) {
         // Q-5 Delete first occurrence of a value
+        int capacity = 6;
+        int[] arr = new int[capacity];
+        int n =5;
+
+        arr[0] = 50;
+        arr[1] = 60;
+        arr[2] = 70;
+        arr[3] = 60;
+        arr[4] = 100;
+
+        int x = 60;
+        int pos = -1;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] == x){
+                pos = i;
+                break;
+            }
+        }
+
+        if(pos != -1) {
+            for(int i = pos; i < n - 1; i++){
+                arr[i] = arr[i + 1];
+            }
+            n--;
+        }
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
+
--- Practice_Arrays/Day_4/Q_6.java
+++ Practice_Arrays/Day_4/Q_6.java
@@ -3,5 +3,31 @@ package Day_4;
 public class Q_6 {
     public static void main(String[] args) {
         // Q-6 Remove all occurrences of a given value
+        int capacity = 10;
+        int[] arr = new int[capacity];
+        int n = 7;
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 60;
+        arr[3] = 60;
+        arr[4] = 50;
+        arr[5] = 60;
+        arr[6] = 70;
+        
+        int x  = 60;
+        int k = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] != x){
+                arr[k] = arr[i];
+                k++;
+            }
+        }
+        n = k;
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        
     }
 }
+
+
