102a35ec10e0a9674ea729ed3ed970be2a365023
Biswajit Rout <routhappy9876@gmail.com>
2026-01-29 05:14:56 +0530

    solved day 6 question one

--- Practice_Arrays/Day_6/day6.java
+++ Practice_Arrays/Day_6/day6.java
@@ -2,7 +2,21 @@ package Day_6;
 
 public class day6 {
     public static void main(String[] args) {
-        
+        // Q-1 Reverse array using two pointers
+        int[] arr = {7, 8, 9, 10};
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
 
