9647113cf20876464205f1ed41248ad6c64ff7ff
Biswajit Rout <routhappy9876@gmail.com>
2026-01-29 08:12:40 +0530

    solve next question of arrayday 6 number 5

--- Practice_Arrays/Day_6/Q_5.java
+++ Practice_Arrays/Day_6/Q_5.java
@@ -3,9 +3,19 @@ package Day_6;
 public class Q_5 {
     public static void main(String[] args) {
         // Q-5 Pair sum equals given number (unsorted)
-        int[] arr = {7,8,9,0,8,50,10};
+        int[] arr = {8, 3, 7, 2, 5};
+        int target = 10;
+        boolean found = false;
         for(int i = 0; i < arr.length; i++) {
-            
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] + arr[j] == target) {
+                    System.out.println("Pair found:" + arr[i] + "+" + arr[j] +  " = " + target );
+                    found = true;
+                }
+            }
+        }
+        if(!found) {
+            System.out.println("No pair found");
         }
     }    
 }
