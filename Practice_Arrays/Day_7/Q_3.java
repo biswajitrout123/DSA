7f06f89686965e2493280142d89fc8b8840818f1
Biswajit Rout <routhappy9876@gmail.com>
2026-01-30 05:53:32 +0530

    solved next question of array sliding window type Qs

---
+++ Practice_Arrays/Day_7/Q_3.java
@@ -0,0 +1,34 @@
+package Day_7;
+
+public class Q_3 {
+
+    public static void main(String[] args) {
+        // Q-3 Find minimum sum subarray of size k
+         int[] arr = { 2, 1, 5, 1, 3, 2 };
+        int k = 3;
+
+        int minSum = 0;
+        int windowSum = 0;
+
+        for(int i = 0; i < k; i++) {
+            minSum = minSum + arr[i];
+        }
+        windowSum = minSum;
+
+        for(int i = k; i < arr.length; i++) {
+            windowSum = windowSum + arr[i] - arr[i - k];
+            minSum = Math.min(windowSum, minSum);
+        }
+        System.out.println(minSum);
+
+    }
+}
+
+
+// Time: O(n)
+// Space: O(1)
+
+
+
+
+
