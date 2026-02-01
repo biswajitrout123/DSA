85c22aba0491083e22a9db563315dd17826f0faf
Biswajit Rout <routhappy9876@gmail.com>
2026-02-01 17:09:57 +0530

    solved day-8 Q-1

--- Practice_Arrays/Day_8/day8.java
+++ Practice_Arrays/Day_8/day8.java
@@ -1,26 +1,24 @@
 package Day_8;
 
+import java.util.*;
+
 public class day8 {
     public static void main(String[] args) {
         // Sort an array (any method)
-        int[] arr = {7,10, 25, 8,9,10};
-        int n = arr.length;
-        for(int i = 0; i < n-1; i++) {
-
+        int[] arr = { 7, 10, 25, 8, 9, 10 };
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
         }
+        System.out.println(Arrays.toString(arr));
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 // 📅 DAY 8 – Sorting Basics
 
@@ -37,4 +35,3 @@ public class day8 {
 // Sort only even numbers in array
 
 // ✅ You should know why sorting helps
-
