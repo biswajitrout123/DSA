2eda8f63e657a6512999d713918414e0e27a1b67
Biswajit Rout <routhappy9876@gmail.com>
2026-02-01 06:23:05 +0530

    update in day7-Q-3

--- Practice_Arrays/Day_7/Q_2.java
+++ Practice_Arrays/Day_7/Q_2.java
@@ -3,11 +3,6 @@ package Day_7;
 public class Q_2 {
     public static void main(String[] args) {
         // Q-2 Find maximum sum of subarray of size k
-        int[] arr = { 2, 1, 5, 1, 3, 2 };
-        int k = 3;
-
-        
-        
 
         // int windowSum = 0;
         // int maxSum = 0;
@@ -23,26 +18,35 @@ public class Q_2 {
         // }
         // System.out.println(maxSum);
 
-
-
-
-
-
-        
         // int windowSum = 0;
         // int maxSum = 0;
         // for (int i = 0; i < k; i++) {
-        //     windowSum += arr[i];
+        // windowSum += arr[i];
         // }
         // maxSum = windowSum;
 
         // for (int i = k; i < arr.length; i++) {
-        //     windowSum = windowSum + arr[i] - arr[i - k];
-        //     maxSum = Math.max(maxSum, windowSum);
+        // windowSum = windowSum + arr[i] - arr[i - k];
+        // maxSum = Math.max(maxSum, windowSum);
         // }
         // System.out.println(maxSum);
 
+        // DONE BY UNDERSTNDING YT-videos
+        int[] arr = { 2, 1, 5, 1, 3, 2 };
+        int k = 3;
+        int wSum = 0;
+        int mSum = Integer.MIN_VALUE;
 
-        
+        for(int i = 0; i < k; i++){
+            wSum = wSum + arr[i];
+        }
+        mSum = wSum;
+
+        for(int i = k; i < arr.length; i++) {
+            wSum = wSum - arr[i-k] + arr[i];
+            mSum = Math.max(wSum, mSum);
+        }
+
+        System.out.println(mSum);
     }
 }
--- Practice_Arrays/Day_7/Q_3.java
+++ Practice_Arrays/Day_7/Q_3.java
@@ -4,31 +4,41 @@ public class Q_3 {
 
     public static void main(String[] args) {
         // Q-3 Find minimum sum subarray of size k
+
+        
+        // int minSum = 0;
+        // int windowSum = 0;
+
+        // for (int i = 0; i < k; i++) {
+        //     minSum = minSum + arr[i];
+        // }
+        // windowSum = minSum;
+
+        // for (int i = k; i < arr.length; i++) {
+        //     windowSum = windowSum + arr[i] - arr[i - k];
+        //     minSum = Math.min(windowSum, minSum);
+        // }
+        // System.out.println(minSum);
+
         int[] arr = { 2, 1, 5, 1, 3, 2 };
         int k = 3;
 
         int minSum = 0;
-        int windowSum = 0;
+        int wSum = 0;
 
         for(int i = 0; i < k; i++) {
             minSum = minSum + arr[i];
         }
-        windowSum = minSum;
+        wSum = minSum;
 
         for(int i = k; i < arr.length; i++) {
-            windowSum = windowSum + arr[i] - arr[i - k];
-            minSum = Math.min(windowSum, minSum);
+            wSum = wSum - arr[i - k] + arr[i];
+            minSum = Math.min(wSum, minSum);
         }
         System.out.println(minSum);
-
     }
 }
 
-
 // Time: O(n)
 // Space: O(1)
-
-
-
-
 
