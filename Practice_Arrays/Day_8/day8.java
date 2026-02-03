be0df955950d66dbefabb3313b50f1f5f8649e1c
Biswajit Rout <routhappy9876@gmail.com>
2026-02-03 06:09:52 +0530

    solved day-8 Q-1

--- Practice_Arrays/Day_8/day8.java
+++ Practice_Arrays/Day_8/day8.java
@@ -3,19 +3,38 @@ package Day_8;
 import java.util.*;
 
 public class day8 {
+    public static void printArray(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         // Sort an array (any method)
+        // for (int i = 0; i < arr.length - 1; i++) {
+        //     for (int j = 0; j < arr.length - i - 1; j++) {
+        //         if (arr[j] > arr[j + 1]) {
+        //             int temp = arr[j];
+        //             arr[j] = arr[j + 1];
+        //             arr[j + 1] = temp;
+        //         }
+        //     }
+        // }
+        // System.out.println(Arrays.toString(arr));
+
+
+
         int[] arr = { 7, 10, 25, 8, 9, 10 };
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j + 1]){
                     int temp = arr[j];
-                    arr[j] = arr[j + 1];
+                    arr[j] = arr[i + 1];
                     arr[j + 1] = temp;
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
+        printArray(arr);
 
     }
 }
