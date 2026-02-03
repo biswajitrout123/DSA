ff5b1bf7cf1de5803be023945952fed5179386fb
Biswajit Rout <routhappy9876@gmail.com>
2026-02-03 06:19:11 +0530

    solved 2nd Q from day-8

---
+++ Practice_Arrays/Day_8/Q_1.java
@@ -0,0 +1,21 @@
+package Day_8;
+
+public class Q_1 {
+    public static void main(String[] args) {
+        // Check if array is already sorted
+        int[] arr = {5,5,5,5,5};
+        boolean sorted = true;
+        for(int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] > arr[i + 1]){
+                sorted = false;
+                break;
+            }
+        }
+        if(sorted) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
