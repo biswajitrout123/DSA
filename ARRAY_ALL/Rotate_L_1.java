316a87ceb41c1424753cff7270b7c7ec3da7eb31
Biswajit Rout <routhappy9876@gmail.com>
2026-02-08 07:36:07 +0530

    solved Q3

---
+++ ARRAY_ALL/Rotate_L_1.java
@@ -0,0 +1,27 @@
+package ARRAY_ALL;
+
+public class Rotate_L_1 {
+    public static void main(String[] args) {
+        // Left Rotate by 1
+        int[] arr = { 7, 8, 9, 10 };
+        int temp = arr[0];              // store first element
+
+        // shift elements to the left
+        for (int i = 0; i < arr.length-1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        // put first element at the end
+        arr[arr.length-1] = temp;
+        
+       // print array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+
+
+
+
+
