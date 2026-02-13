559ba1297451717dfb9524329983dfa21489d5b7
Biswajit Rout <routhappy9876@gmail.com>
2026-02-13 10:06:58 +0530

    solved next

---
+++ HashMap_YT_SHADHA/Itinerary_Tickets.java
@@ -0,0 +1,61 @@
+package HashMap_YT_SHADHA;
+
+import java.util.HashMap;
+
+public class Itinerary_Tickets {
+
+    public static String getStart(HashMap<String, String> tick){
+        HashMap<String, String> revMap = new HashMap<>();
+
+        for(String key : tick.keySet()) {
+            revMap.put(tick.get(key), key);
+        }
+        for(String key : tick.keySet()) {
+            if(!revMap.containsKey(key)) {
+                return key;
+            }
+        }
+
+        return null;
+
+    }
+    public static void main(String[] args) {
+        // FIND ITINERARY FROM TICKETS
+        HashMap<String, String> tickets = new HashMap<>();
+        tickets.put("Chennai", "Bengaluru");
+        tickets.put("Mumbai", "Delhi");
+        tickets.put("Goa", "Chennai");
+        tickets.put("Delhi", "Goa");
+
+        String start = getStart(tickets);
+
+        while (tickets.containsKey(start)) {
+            System.out.print(start + "->");
+            start = tickets.get(start);
+        }
+
+        System.out.println(start);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
