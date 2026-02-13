8d83fff88b3ffbe864883a6443cca041520ec709
Biswajit Rout <routhappy9876@gmail.com>
2026-02-13 08:31:02 +0530

    solved Hash set q

---
+++ HashMap_YT_SHADHA/Union2Array.java
@@ -0,0 +1,26 @@
+package HashMap_YT_SHADHA;
+
+import java.util.HashSet;
+
+public class Union2Array {
+    public static void main(String[] args) {
+        // UNION OF TWO ARRAYS!
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr1 = {7, 3, 9};
+        int[] arr2 = {6, 3, 9, 2, 9, 4};
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        for(int i = 0; i < n1; i++) {
+            set.add(arr1[i]);
+        }
+        for(int j = 0; j < n2; j++) {
+            set.add(arr2[j]);
+        }
+
+        System.out.println(set);
+        System.out.println(set.size());
+
+    }
+}
+
