59d0fc00e6257b8c970cd3b88968f8ef5a55f6ed
Biswajit Rout <routhappy9876@gmail.com>
2026-02-18 07:48:01 +0530

    update in sort q

--- Day_Wise/Day_10/Day_8/day8.java
+++ Day_Wise/Day_10/Day_8/day8.java
@@ -1,44 +1,3 @@
-package Day_8;
-
-import java.util.*;
-
-public class day8 {
-    public static void printArray(int arr[]){
-        for(int i = 0; i < arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        // Sort an array (any method)
-        // for (int i = 0; i < arr.length - 1; i++) {
-        //     for (int j = 0; j < arr.length - i - 1; j++) {
-        //         if (arr[j] > arr[j + 1]) {
-        //             int temp = arr[j];
-        //             arr[j] = arr[j + 1];
-        //             arr[j + 1] = temp;
-        //         }
-        //     }
-        // }
-        // System.out.println(Arrays.toString(arr));
-
-
-
-        int[] arr = { 7, 10, 25, 8, 9, 10 };
-        for(int i = 0; i < arr.length-1; i++) {
-            for(int j = 0; j < arr.length - i - 1; j++) {
-                if(arr[j] > arr[j + 1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-        printArray(arr);
-
-    }
-}
-
 // 📅 DAY 8 – Sorting Basics
 
 // 🎯 Goal: Sorting logic (not speed)
@@ -54,3 +13,29 @@ public class day8 {
 // A-5 Sort only even numbers in array
 
 // ✅ You should know why sorting helps
+
+
+
+package Day_8;
+
+public class day8 {
+
+    public static void main(String[] args) {
+        int[] nums = {6,5,2,8,9,4};
+        int size = nums.length;
+
+        for(int i = 0; i < size - 1; i++) {
+            for(int j = 0; j < size - i - 1; j++) {
+                if(nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            } 
+
+        }
+        for(int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
