5aa9f4d0fb9f5fe7d8a445af3ff189adf9f6d9f8
Biswajit Rout <routhappy9876@gmail.com>
2026-02-20 05:53:29 +0530

    solve frequency count with hash map

---
+++ Day_Wise/Day_10/Day_9/Q_3.java
@@ -0,0 +1,27 @@
+package Day_9;
+
+import java.util.HashMap;
+
+public class Q_3 {
+    public static void main(String[] args) {
+        // 3. Count frequency of each element
+
+        int[] arr = { 8, 9, 0, 10, 5, 6, 8, 7, 9, 40, 10, 8 };
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : arr) {
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            }
+            else {
+                map.put(num, 1);
+            }
+        }
+        for(int key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
+        }
+
+    }
+}
+
---
+++ Day_Wise/Day_10/Day_9/Q_4.java
@@ -0,0 +1,19 @@
+package Day_9;
+
+public class Q_4 {
+    public static void main(String[] args) {
+    // 4. Find element with highest frequency
+    int[] arr = {8,9,0,10,5,6,8,7,9,40,10,8};
+    int[] freq = new int[arr.length];
+    int visited = -1;
+
+    for(int i = 0; i < arr.length; i++) {
+        int count = 1;
+        
+    }
+
+
+    }
+}
+
+
