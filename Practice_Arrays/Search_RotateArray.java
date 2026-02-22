5b3e6c8cef13f7a7bd7c5921824881cabb7803f0
Biswajit Rout <routhappy9876@gmail.com>
2026-02-22 06:36:38 +0530

    solved dsa search in rotate array

--- Practice_Arrays/Search_RotateArray.java
+++ Practice_Arrays/Search_RotateArray.java
@@ -1,7 +1,39 @@
 public class Search_RotateArray {
 
+    public static int search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length-1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == target) {
+                return mid;
+            }
+            if(arr[low] <= arr[mid]) {
+                if(target >= arr[low] && target < arr[mid]) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            else if (target > arr[mid] && target <= arr[high]) {
+                low = mid + 1;                
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         // search in rotated sorted array
-        
+        int[] arr = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 0;
+        int index = search(arr, target);
+        System.out.println(index);
+
     }
 }
