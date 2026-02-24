6c6e76175edd8445b24efd8a58366f0ee6b79cc7
Biswajit Rout <routhappy9876@gmail.com>
2026-02-24 20:42:34 +0530

    solved hashmap Qs

--- ARRAY_ALL/Count_Frequency.java
+++ ARRAY_ALL/Count_Frequency.java
@@ -2,16 +2,19 @@ import java.util.HashMap;
 
 public class Count_Frequency {
     public static void main(String[] args) {
-        int[] arr = {4,4,5,5,3,3,2,2,2,3,8,9,7,10};
+        int[] arr = { 4, 4, 5, 5, 3, 3, 2, 2, 2, 3, 8, 9, 7, 10 };
         HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
 
-        
+        for(int key : map.keySet()) {
+            System.out.println(key + "->" + map.get(key));
+        }
+
     }
 }
-
-
-
-
-
-
-
