0471a8be0e2a6f67a4ec08d5c67a0113725fd589
Biswajit Rout <routhappy9876@gmail.com>
2026-02-25 07:11:23 +0530

    solbed pair sum by hashset

---
+++ HashMap_YT_SHADHA/PairWithSum.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class PairWithSum {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        
+
+
+    }
+}
