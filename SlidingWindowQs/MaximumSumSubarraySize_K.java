1792cfe2a944b9dd50860a47e8d13ad234094b77
Biswajit Rout <routhappy9876@gmail.com>
2026-02-28 20:14:42 +0530

    update in dsa poblem

--- SlidingWindowQs/MaximumSumSubarraySize_K.java
+++ SlidingWindowQs/MaximumSumSubarraySize_K.java
@@ -2,6 +2,18 @@ package SlidingWindowQs;
 
 public class MaximumSumSubarraySize_K {
     public static void main(String[] args) {
+        int arr[] = {2, 1, 5, 1, 3, 2};
+        int wSum = 0;
+        int mSum = Integer.MIN_VALUE;
+        int k = 3;
+        for(int i = 0; i < k; i++) {
+            wSum = wSum + arr[i];
+        }
+        mSum = wSum;
+
+
         
     }
 }
+
+
