f187cd96ab1eab560b26a7152c5e5b9b4d636d43
Biswajit Rout <routhappy9876@gmail.com>
2026-03-02 06:52:37 +0530

    solved number that appears once, and other numbers twice.

---
+++ ARRAY_ALL/number_that_appears_once_other_numbers_twice.java
@@ -0,0 +1,15 @@
+public class number_that_appears_once_other_numbers_twice {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 2, 1, 2};
+
+        int result = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            result = result ^ arr[i];
+        }
+
+        System.out.println(result);
+    }
+}
+
+
