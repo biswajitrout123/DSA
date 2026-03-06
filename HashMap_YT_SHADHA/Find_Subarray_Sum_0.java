740c01a79a71c7b9dc2ae7fddac0c0af3cd33163
Biswajit Rout <routhappy9876@gmail.com>
2026-03-06 08:38:47 +0530

    solved Subarray With Sum = 0

--- HashMap_YT_SHADHA/Find_Subarray_Sum_0.java
+++ HashMap_YT_SHADHA/Find_Subarray_Sum_0.java
@@ -6,6 +6,16 @@ public class Find_Subarray_Sum_0 {
         int arr[] = {4, 2, -3, 1, 6};
 
         HashSet<Integer> set = new HashSet<>();
-        
+        int k = 3;
+        int sum = 0;
+        for(int num : arr) {
+            sum += num;
+            if(sum == 0 || set.contains(sum)) {
+                System.out.println("Subarray with sum 0 exists");
+                return;
+            }
+            set.add(sum);
+        }
+        System.out.println("No subarray with sum 0");        
     }
 }
