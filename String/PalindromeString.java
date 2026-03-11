8afeda46b7ae82eec34891c4243c15d43da89406
Biswajit Rout <routhappy9876@gmail.com>
2026-03-11 18:38:30 +0530

    solved Palindrome String

--- String/PalindromeString.java
+++ String/PalindromeString.java
@@ -2,10 +2,20 @@ package String;
 
 public class PalindromeString {
     public static void main(String[] args) {
-        String str = "1221";
+        String str = "level";
         int n = str.length();
-        for(int i = 0; i < n; i++) {
-            
+        String rev = "";
+        boolean isPalindrome = true;
+
+        for(int i = n - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        if (str.equals(rev)) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not Pallindrome");
         }
     }
 }
