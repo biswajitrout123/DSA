e7601d8308375c5bca1fc9a12107a62f09323b24
Biswajit Rout <routhappy9876@gmail.com>
2026-03-11 18:24:20 +0530

    solved Reverse String

---
+++ String/PalindromeString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String str = "1221";
+        int n = str.length();
+        for(int i = 0; i < n; i++) {
+            
+        }
+    }
+}
--- String/ReverseString.java
+++ String/ReverseString.java
@@ -4,6 +4,9 @@ public class ReverseString {
 
     public static void main(String[] args) {
         String str = "biswajit rout";
-        
+        int n = str.length();
+        for(int i = n - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
     }
 }
