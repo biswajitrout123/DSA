d35247c466899555302885610c0a30d6c72ba5c6
Biswajit Rout <routhappy9876@gmail.com>
2026-03-19 05:28:19 +0530

    solved Find First Repetating Character in String

--- Placements/Strings_All/Find_First_Repeting_Chararacter.java
+++ Placements/Strings_All/Find_First_Repeting_Chararacter.java
@@ -5,26 +5,16 @@ public class Find_First_Repeting_Chararacter {
     public static void main(String[] args) {
         // Find First Repetating Character
         String str = "programming";
-        
+        boolean[] seen = new boolean[256];
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
 
-        
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-        
+            if(seen[ch]) {
+                System.out.println("First Repeating Character: " + ch);
+                return;
+            }
+            seen[ch] = true;
+        }
+        System.out.println("No Repeating Character");        
     }
 }
