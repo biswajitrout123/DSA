027f33ab6c8ed2c7ce2f14a01ef4870e6caf2f77
Biswajit Rout <routhappy9876@gmail.com>
2026-03-24 06:38:23 +0530

    Solved Striver 2nd Q of Find Second largest

---
+++ StriverSheetsPractice/Array/Second_Largest.java
@@ -0,0 +1,20 @@
+package StriverSheetsPractice.Array;
+
+public class Second_Largest {
+    public static void main(String[] args) {
+        int[] arr = { 8, 8, 7, 6, 5 };
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println(largest);
+        System.out.println(secondLargest);
+    }
+}
