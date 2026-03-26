93a29c75dc0f115ae3d1c1d8c834e153a1807ab6
Biswajit Rout <routhappy9876@gmail.com>
2026-03-26 17:24:13 +0530

    Practicing Basics Array Qs

---
+++ StriverSheetsPractice/Array/Union_of_two_sorted_arrays.java
@@ -0,0 +1,21 @@
+package StriverSheetsPractice.Array;
+
+import java.util.ArrayList;
+
+public class Union_of_two_sorted_arrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 7};
+
+        ArrayList<Integer> result = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        for(int i = 0; i < arr1.length; i++) {
+            if(arr1[i] == arr2[i]) {
+                
+            }
+
+        }
+
+    }
+}
