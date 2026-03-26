36c041525ac795fe7506734ce3469887755892ba
Biswajit Rout <routhappy9876@gmail.com>
2026-03-26 05:39:39 +0530

    Solved Striver 4th Qs Remove_duplicates_from_sorted_array

---
+++ StriverSheetsPractice/Array/Remove_duplicates_from_sorted_array.java
@@ -0,0 +1,19 @@
+package StriverSheetsPractice.Array;
+
+public class Remove_duplicates_from_sorted_array {
+
+    public static void main(String[] args) {
+        int[] arr = {0, 0, 3, 3, 5, 6};
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[index]) {
+                index++;
+                arr[index] = arr[i];
+            }
+        }
+        System.out.println(index + 1);
+        for(int k = 0; k <= index; k++){
+            System.out.print(arr[k] + " ");
+        }
+    }
+}
