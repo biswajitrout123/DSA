229aeeccff899ea139d038a72e890d14a73fadf5
Biswajit Rout <routhappy9876@gmail.com>
2026-03-27 22:12:11 +0530

    Solved Striver Qs Missing Number

--- StriverSheetsPractice/Array/Second_Largest.java
+++ StriverSheetsPractice/Array/Second_Largest.java
@@ -6,11 +6,10 @@ public class Second_Largest {
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > largest) {
+            if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
-            }
-            else if(arr[i] < largest && arr[i] > secondLargest) {
+            } else if (arr[i] < largest && arr[i] > secondLargest) {
                 secondLargest = arr[i];
             }
         }
