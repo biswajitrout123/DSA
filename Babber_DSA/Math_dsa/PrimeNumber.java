7b293d2281c013d3f2ede389481791159665e93f
Biswajit Rout <routhappy9876@gmail.com>
2026-04-15 18:34:58 +0530

    revising dsa solved Prime Number

---
+++ Babber_DSA/Math_dsa/PrimeNumber.java
@@ -0,0 +1,21 @@
+package Babber_DSA.Math_dsa;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 29;
+        boolean isPrime = true;
+
+        for (int i = 2; i * i <= n; i++) {
+             if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+            
+        }
+        if (isPrime) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not Prime Number");
+        }
+    }
+}
