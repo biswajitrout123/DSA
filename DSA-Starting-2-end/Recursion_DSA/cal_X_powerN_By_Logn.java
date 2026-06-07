e9ebaee7885f6cc41d5e8d71ddf5d81468e535e5
Biswajit Rout <routhappy9876@gmail.com>
2026-06-07 07:43:14 +0530

    Solved DSA Print X-to-the-power-n by recursive function in java log n time complixity

---
+++ DSA-Starting-2-end/Recursion_DSA/cal_X_powerN_By_Logn.java
@@ -0,0 +1,29 @@
+package Recursion_DSA;
+
+public class cal_X_powerN_By_Logn {
+    public static int calculatePower(int x, int n) {
+        if(n == 0) {
+            return 1;
+        }
+        if(x == 0) {
+            return 0;
+        }
+        int halfPower = calculatePower(x, n/2);
+
+        if(n % 2 == 0) {
+            return halfPower  * halfPower;
+            // or - return calculatePower(x, n/2) * calculatePower(x, n/2) * x;
+        }
+        else {
+            return halfPower * halfPower * x;
+            // or - calculatePower(x, n/2) * calculatePower(x, n/2) * x;
+        }
+    }
+    public static void main(String[] args) {
+        int x = 2;
+        int n = 5;
+        int ans = calculatePower(x, n);
+        System.out.println(ans);
+        
+    }
+}
