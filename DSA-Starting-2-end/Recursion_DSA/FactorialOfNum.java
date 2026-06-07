f7d99b3cffc67194e32094f6c13d2d2e0afb64b9
Biswajit Rout <routhappy9876@gmail.com>
2026-06-07 05:56:53 +0530

    Solving dsa with java Solved Factorial of a numebr with recursive

---
+++ DSA-Starting-2-end/Recursion_DSA/FactorialOfNum.java
@@ -0,0 +1,23 @@
+package Recursion_DSA;
+
+public class FactorialOfNum {
+    public static int calcFactorial(int n) {
+
+        if(n == 1 || n == 0) {
+            return 1;
+        }
+        int fact_nm1 = calcFactorial(n - 1);
+        int fact_n = n * fact_nm1;
+        return fact_n;
+
+
+
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = calcFactorial(n);
+        System.out.print(ans);
+        
+    }
+}
