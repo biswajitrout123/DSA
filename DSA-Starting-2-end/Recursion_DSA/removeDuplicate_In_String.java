13e6e3a873b8021b0821abc0bf7c50578d127a74
Biswajit Rout <routhappy9876@gmail.com>
2026-06-08 09:03:24 +0530

    Solved DSA Remove-Duplicates by Recursion in java

---
+++ DSA-Starting-2-end/Recursion_DSA/removeDuplicate_In_String.java
@@ -0,0 +1,25 @@
+public class removeDuplicate_In_String {
+    public static boolean[] map = new boolean[26];
+    public static void removeDuplicates(String str, int idx, String newString){
+        if(idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a']) {
+            removeDuplicates(str, idx+1, newString);            
+        }
+        else{
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx+1, newString);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        String str = "abbccda";
+        removeDuplicates(str, 0, "");
+
+    }
+}
