813afc347d0ac4588161bca62ccc94b053c01610
Biswajit Rout <routhappy9876@gmail.com>
2026-06-09 07:41:01 +0530

    Solved DSA print-SubSequences of String by Recursion in java

---
+++ DSA-Starting-2-end/Recursion_DSA/printUniqueSubSequences.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class printUniqueSubSequences {
+    public static void uniqueSubSequences(String str, int idx, String newString, HashSet<String> set) {
+
+        if(idx == str.length()) {
+            if(set.contains(newString)){
+                return;
+            }
+            else {
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+        char currChar = str.charAt(idx);
+
+        uniqueSubSequences(str, idx+1, newString+currChar, set);
+        uniqueSubSequences(str, idx+1, newString, set);
+
+
+
+    }
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        String str = "aaa";
+        uniqueSubSequences(str, 0, "", set);
+        
+    }
+    
+}
