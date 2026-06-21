d9843e1ac160a0318ee878ef4a7eb360c74e3111
Biswajit Rout <routhappy9876@gmail.com>
2026-06-21 16:22:02 +0530

    Solved DSA Count-Number-of-Element-in-Array by Recrusion in java

---
+++ DSA-Starting-2-end/Recursion_DSA/count_Element_in_Array.java
@@ -0,0 +1,20 @@
+package Recursion_DSA;
+
+public class count_Element_in_Array {
+    public static int countElment(int[] arr, int idx, int target, int count) {
+        if(idx == arr.length) {
+            return count;
+        }
+        if(arr[idx] == target) {
+            count = count + 1;
+        }
+        return countElment(arr, idx + 1, target, count);
+    }
+    public static void main(String[] args) {
+        int[] arr = {6,7,8,9,10,10,10,10,10,10,10,10,10,10};
+        int target = 10;
+        int count = countElment(arr, 0, target, 0);
+        System.out.println(count);
+
+    }
+}
