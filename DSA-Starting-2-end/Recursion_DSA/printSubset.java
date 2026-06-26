9f27efa735831c24e6cb5f8c6b963e89ce78fed4
Biswajit Rout <routhappy9876@gmail.com>
2026-06-26 07:26:19 +0530

    Solved DSA Print-SubSet-In-Array by Recrusion in java

--- DSA-Starting-2-end/Recursion_DSA/printSubset.java
+++ DSA-Starting-2-end/Recursion_DSA/printSubset.java
@@ -1,8 +1,23 @@
 package Recursion_DSA;
 
+import java.util.ArrayList;
+
 public class printSubset {
+    public static void printSubset(int[] arr, int idx, ArrayList<Integer> curr) {
+        if(idx == arr.length) {
+            System.out.println(curr);
+            return;
+        }
+
+        curr.add(arr[idx]);
+        printSubset(arr, idx + 1, curr);
+        curr.remove(curr.size() - 1);
+
+        printSubset(arr, idx+1, curr);
+    }
 
     public static void main(String[] args) {
         int[] arr = {1,2,3};
+        printSubset(arr, 0, new ArrayList<>());
     }
 }
