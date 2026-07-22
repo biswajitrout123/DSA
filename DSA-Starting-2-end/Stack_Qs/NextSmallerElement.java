90b374ef77617170c8c50bc4fd42b765e34ce142
Biswajit Rout <routhappy9876@gmail.com>
2026-07-22 06:35:36 +0530

    Solving Stack Questions and done Next-Smaller-Element

---
+++ DSA-Starting-2-end/Stack_Qs/NextSmallerElement.java
@@ -0,0 +1,23 @@
+package Stack_Qs;
+import java.util.*;
+
+public class NextSmallerElement {
+    public static void main(String[] args) {
+        int[] arr = {4,8,5,2,25};
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[arr.length];
+        for(int i = arr.length -1; i >= 0; i--) {
+            while(!stack.isEmpty() && stack.peek() >= arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+}
