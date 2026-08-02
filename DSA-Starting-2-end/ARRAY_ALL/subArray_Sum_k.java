8464b22aa65f31839068110bf67aa619ed2fb81a
Biswajit Rout <routhappy9876@gmail.com>
2026-08-02 11:45:41 +0530

    Solved DSA SubArray-Sum-K

---
+++ DSA-Starting-2-end/ARRAY_ALL/subArray_Sum_k.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class subArray_Sum_k {
+    public static void main(String[] args) {
+        int[] arr = {9, 4, 20, 3, 10, 5};
+
+        int k = 33;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for(int i = 1; i < arr.length; i++) {
+            sum += arr[i];
+            if(sum == k) {
+                maxLen = i + 1;
+            }
+            int rem = sum - k;
+
+            if(map.containsKey(rem)) {
+                int len = i - map.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+            if(!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        System.out.println("Longest Length = " + maxLen);        
+    }
+}
