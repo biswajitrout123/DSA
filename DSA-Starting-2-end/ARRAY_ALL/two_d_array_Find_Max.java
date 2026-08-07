14a4d03881fc598eecd29c0eec9b423c659b60bc
Biswajit Rout <routhappy9876@gmail.com>
2026-08-07 05:51:07 +0530

    Solved DSA-ARRAY-Find-Maxmimum_Number

---
+++ DSA-Starting-2-end/ARRAY_ALL/two_d_array_Find_Max.java
@@ -0,0 +1,14 @@
+public class two_d_array_Find_Max {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3}, {21,20,19}};
+        int maxValue = arr[0][0];
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] > maxValue){
+                    maxValue = arr[i][j];
+                }
+            }
+        }
+        System.out.println(maxValue);
+    }
+}
