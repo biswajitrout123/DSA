9110e63e63b55e30d613553d325333cb3edb498e
Biswajit Rout <routhappy9876@gmail.com>
2026-08-13 20:09:40 +0530

    Solved DSA java Triplate_sum_target

---
+++ DSA-Starting-2-end/ARRAY_ALL/triplateSum_Target.java
@@ -0,0 +1,19 @@
+public class triplateSum_Target {
+    public static void main(String[] args) {
+
+        int[] arr = {1,4,5,6,3};
+        int target = 12;
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                for(int k = j + 1; k < arr.length; k++) {
+                    if(arr[i] + arr[j] + arr[k] == target) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
